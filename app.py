2693bf3333a47011ef37e348648f86d7cac5c9f6
Arshwin Sajeevan <arshwin619@gmail.com>
2025-10-29 22:47:06 +0530

    Final commit

--- app.py
+++ app.py
@@ -372,6 +372,8 @@ def about():
 
 # ========== RUN ==========
 if __name__ == "__main__":
+    import os
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
